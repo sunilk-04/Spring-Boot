e9f24f40ba6f1f287ef51f5b7a5d7ac59a34d22d
sunilk-04 <sunilk@prominentpixel.com>
2022-07-13 18:33:36 +0530

    Added code used for deployment

--- thymeleafdemo/src/main/java/com/prominentpixel/springboot/thymeleafdemo/ThymeleafdemoApplication.java
+++ thymeleafdemo/src/main/java/com/prominentpixel/springboot/thymeleafdemo/ThymeleafdemoApplication.java
@@ -2,12 +2,20 @@ package com.prominentpixel.springboot.thymeleafdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ThymeleafdemoApplication {
+public class ThymeleafdemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThymeleafdemoApplication.class, args);
+	}
+
+	// Below method will be used for deployment
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ThymeleafdemoApplication.class);
 	}
 
 }
