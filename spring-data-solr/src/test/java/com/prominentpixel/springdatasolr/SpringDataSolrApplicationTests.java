383c54f572648ea8f07989475c2ff64273391447
sunilk-04 <sunilk@prominentpixel.com>
2022-07-18 13:55:07 +0530

    Added files

---
+++ spring-data-solr/src/test/java/com/prominentpixel/springdatasolr/SpringDataSolrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prominentpixel.springdatasolr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataSolrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
