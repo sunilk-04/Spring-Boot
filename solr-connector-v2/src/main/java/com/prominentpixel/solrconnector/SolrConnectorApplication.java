8852be8ec0a02c7e0e7ea25c87d0305f58e2e590
sunilk-04 <sunilk@prominentpixel.com>
2022-07-22 13:06:46 +0530

    Added version 2 of Solr connector

---
+++ solr-connector-v2/src/main/java/com/prominentpixel/solrconnector/SolrConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.prominentpixel.solrconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolrConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolrConnectorApplication.class, args);
+	}
+
+}
---
+++ solr-connector-v2/src/test/java/com/prominentpixel/solrconnector/SolrConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prominentpixel.solrconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolrConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
