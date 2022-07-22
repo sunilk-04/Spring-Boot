b490be30cde5f6661d3676c147c324ca68285ca2
sunilk-04 <sunilk@prominentpixel.com>
2022-07-22 16:24:18 +0530

    Fixed some issue related to URL

--- solr-connector-v2/src/main/java/com/prominentpixel/solrconnector/SolrConnectorApplication.java
+++ solr-connector-v2/src/main/java/com/prominentpixel/solrconnector/SolrConnectorApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SolrConnectorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SolrConnectorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SolrConnectorApplication.class, args);
+    }
 
 }
